f34eac4a7051bd67c22c45386c4937544ca28a91
toshi0607 <s.toshi0607@gmail.com>
2020-08-02 23:05:36 +0900

    Add google_compute_target_https_proxy

--- three-tier/main.tf
+++ three-tier/main.tf
@@ -151,7 +151,48 @@ resource "google_compute_url_map" "default" {
   default_service = google_compute_backend_service.backend1.self_link
 }
 
+resource "google_compute_target_https_proxy" "default" {
+  name             = "default"
+  url_map          = google_compute_url_map.default.self_link
+  ssl_certificates = [google_compute_ssl_certificate.example.self_link]
+}
+
+resource "google_compute_ssl_certificate" "example" {
+  name        = "example"
+  private_key = tls_private_key.example.private_key_pem
+  certificate = tls_self_signed_cert.example.cert_pem
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/self_signed_cert
+resource "tls_self_signed_cert" "example" {
+  key_algorithm   = tls_private_key.example.algorithm
+  private_key_pem = tls_private_key.example.private_key_pem
+
+  # Certificate expires after 12 hours.
+  validity_period_hours = 12
+
+  # Generate a new certificate if Terraform is run within three
+  # hours of the certificate's expiration time.
+  early_renewal_hours = 3
+
+  # Reasonable set of uses for a server SSL certificate.
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+
+  dns_names = ["example.com", "example.net"]
+
+  subject {
+    common_name  = "example.com"
+    organization = "ACME Examples, Inc"
+  }
+}
+
 # google_compute_global_forwarding_rule
-# google_compute_target_https_proxy
-# google_compute_ssl_certificate
-# google_compute_global_address => Reservedなやつでなくていい
