5c3f1e8913d6f499e53a776f0af4e367d797b66e
toshi0607 <s.toshi0607@gmail.com>
2020-09-06 09:21:11 +0900

    Add variable sample

---
+++ variable/main.tf
@@ -0,0 +1,26 @@
+variable "compute_instance_zone" {
+  type        = string
+  description = "A zone used in compute instance"
+  default     = "asia-northeast1-a"
+
+  validation {
+    condition     = contains(["asia-northeast1-a", "asia-northeast1-b", "asia-northeast1-c"], var.compute_instance_zone)
+    error_message = "The compute_instance_zone must be in asia-northeast1 region."
+  }
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = var.compute_instance_zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
