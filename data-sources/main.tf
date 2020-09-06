813484db0d20ffd32651e9e5f8c5801563743c3c
toshi0607 <s.toshi0607@gmail.com>
2020-09-06 10:40:03 +0900

    Add data sources sample

---
+++ data-sources/main.tf
@@ -0,0 +1,20 @@
+data "google_compute_zones" "available" {
+  region = "asia-northeast1"
+  status = "UP"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = data.google_compute_zones.available.names[0]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
