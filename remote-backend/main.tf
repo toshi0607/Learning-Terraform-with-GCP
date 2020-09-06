1daa20ac22b40893233fb2d4428f2c9f0f9756b1
toshi0607 <s.toshi0607@gmail.com>
2020-09-06 11:15:57 +0900

    Add remote backend sample

---
+++ remote-backend/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "gcs" {
+    # 【YOUR BUCKET】を作成したバケット名に置き換えてください。
+    bucket  = 【YOUR BUCKET】
+    prefix = "terraform/state"
+  }
+
+  required_providers {
+    google = ">= 3.33.0"
+  }
+}
+
+provider "google" {
+  # 【YOUR PROJECT】をあなたのGCPプロジェクトに置き換えてください。
+  project = 【YOUR PROJECT】
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
