8698bff2b8432094eef63f7b54f2a7e0f6376a9c
Toshinori Sugita <s.toshi0607@gmail.com>
2022-08-28 15:52:29 +0900

    Update locals

--- locals/README.md
+++ locals/README.md
@@ -4,8 +4,9 @@ Terraform 0.13.0 or later is required.
 ```shell
 $ gcloud auth application-default login
 
+# replace 【YOUR PROJECT】 with your project
+# e.g.
 # export GOOGLE_PROJECT=terraform-toshi0607
-# 【YOUR PROJECT】をあなたのGCPプロジェクトに置き換えてください。
 $ export GOOGLE_PROJECT=【YOUR PROJECT】
 
 $ terraform init
--- locals/main.tf
+++ locals/main.tf
@@ -25,13 +25,13 @@ locals {
 resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "f1-micro"
-  zone         = "asia-northeast1-a"
+  zone         = "asia-northeast1-c"
 
   labels = local.common_labels
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
 
